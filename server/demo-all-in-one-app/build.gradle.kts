9e706ea8a0ace448ed758bb0ef9a47d3d215f056
jini <jini@jiniworld.me>
2022-12-02 13:35:12 +0900

    :heavy_plus_sign: h2 database dependency 추가

--- server/demo-all-in-one-app/build.gradle.kts
+++ server/demo-all-in-one-app/build.gradle.kts
@@ -4,9 +4,11 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
     implementation("com.epages:restdocs-api-spec-model:0.16.2")
+    testImplementation("org.mockito:mockito-inline")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    runtimeOnly("com.h2database:h2")
 
     implementation(project(":util:common-util"))
 }
