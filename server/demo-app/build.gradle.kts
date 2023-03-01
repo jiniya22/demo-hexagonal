8f666972911bb31587fd7fe7c3cc1597f6c88967
jini <jini@jiniworld.me>
2023-03-01 17:38:26 +0900

    :arrow_up: [demo-app] springdoc-openapi-ui version upgrade 1.6.14

--- server/demo-app/build.gradle.kts
+++ server/demo-app/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     compileOnly("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
 
     implementation(project(":util:common-util"))
     implementation(project(":core:demo-core"))
