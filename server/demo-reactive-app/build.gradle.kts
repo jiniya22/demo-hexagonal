c675c1041723768aa0ae90c96239d1550da26391
jini <jini@jiniworld.me>
2023-03-07 23:20:11 +0900

    :wrench:

--- server/demo-reactive-app/build.gradle.kts
+++ server/demo-reactive-app/build.gradle.kts
@@ -11,7 +11,8 @@ dependencies {
 
     implementation(project(":util:common-util"))
     implementation(project(":core:demo-reactive-core"))
-    implementation(project(":infrastructure:datastore-mariadb-reactive"))
+    implementation(project(":infrastructure:datastore-mongodb-reactive"))
 
-    implementation("io.netty:netty-resolver-dns-native-macos:4.1.86.Final:osx-aarch_64")
+//    implementation(project(":infrastructure:datastore-mariadb-reactive"))
+//    implementation("io.netty:netty-resolver-dns-native-macos:4.1.86.Final:osx-aarch_64")
 }
