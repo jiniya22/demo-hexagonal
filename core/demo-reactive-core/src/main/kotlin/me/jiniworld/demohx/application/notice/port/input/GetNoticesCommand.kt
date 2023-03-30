59cf934058936b59c0f1ae50dfdfa37139d5977d
jini <jini@jiniworld.me>
2023-03-31 02:51:50 +0900

    :art: GetNoticesCommand validation 설정 추가

--- core/demo-reactive-core/src/main/kotlin/me/jiniworld/demohx/application/notice/port/input/GetNoticesCommand.kt
+++ core/demo-reactive-core/src/main/kotlin/me/jiniworld/demohx/application/notice/port/input/GetNoticesCommand.kt
@@ -1,6 +1,8 @@
 package me.jiniworld.demohx.application.notice.port.input
 
+import javax.validation.constraints.Min
+
 data class GetNoticesCommand(
-    val page: Int,
-    val size: Int,
+    @Min(0) val page: Int,
+    @Min(5) val size: Int,
 )
--- server/demo-app/src/main/kotlin/me/jiniworld/demohx/web/notice/GetNoticeController.kt
+++ server/demo-app/src/main/kotlin/me/jiniworld/demohx/web/notice/GetNoticeController.kt
@@ -6,8 +6,10 @@ import me.jiniworld.demohx.annotation.WebAdapter
 import me.jiniworld.demohx.application.notice.port.input.GetNoticeQuery
 import me.jiniworld.demohx.application.notice.port.input.GetNoticesCommand
 import me.jiniworld.demohx.model.NotFoundException
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
+@Validated
 @WebAdapter
 @Tag(name = "setting-system", description = "설정-시스템(공지사항, FAQ, 이용약관, 메타정보 등)")
 @RestController
