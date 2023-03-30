7d3b445303f97bb3fd232331b6b0fd1744e02001
jini <jini@jiniworld.me>
2023-03-31 02:50:28 +0900

    :heavy_plus_sign: hibernate-validator 의존성 추가

--- core/demo-reactive-core/build.gradle.kts
+++ core/demo-reactive-core/build.gradle.kts
@@ -2,6 +2,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.data:spring-data-commons")
     implementation("org.springframework:spring-tx")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
+
 
     implementation(project(":util:common-util"))
 }
